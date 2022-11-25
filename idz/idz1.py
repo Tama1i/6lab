2ebd49579a4c64a69c2af4ff4269b3b249b7a779
Tama1i <ryazantseff.matvei@yandex.ru>
2022-11-25 18:53:32 +0300

    fixx

--- idz/idz1.py
+++ idz/idz1.py
@@ -9,11 +9,10 @@ if __name__ == '__main__':
     c = input("Первый символ: ")
     b = input("Второй символ: ")
 
-
-    for i in range(len(s)):
-        if(c in s[i]): 
-            print(s[i])
-        elif(b in s[i]):
-            print(s[i])
+    for i,val in enumerate(s):
+        if c in val: 
+            print(val)
+        elif b in val:
+            print(val)
 
 
